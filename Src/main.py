8d401eb04d0702eb13fbedad47b47c72aa198f53
StroepWafel <109832156+NIDNHU@users.noreply.github.com>
2025-03-04 19:20:31 +1030

    Update main.py - && to "and"
    line 21, @ar-cyber accidentally wrote && instead of and and i failed to notice when pushing, causing pylint error, has now been fixed

--- Src/main.py
+++ Src/main.py
@@ -18,7 +18,7 @@ YOUTUBE_VIDEO_ID = config["YOUTUBE_VIDEO_ID"]
 RATE_LIMIT_SECONDS = config['RATE_LIMIT_SECONDS']
 VLC_PATH = config['VLC_PATH']
 FFMPEG_PATH = config['FFMPEG_PATH']
-if FFMPEG_PATH == "" && "Linux" in platform.platform():
+if FFMPEG_PATH == "" and "Linux" in platform.platform():
     FFMPEG_PATH = "/usr/bin/ffmpeg"
 PREFIX = config['PREFIX']
 user_last_command = defaultdict(lambda: 0)
